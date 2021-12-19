c6247a867d277215d7addb70afe20787645da91b
Wae-ird <vuvubusiness@gmail.com>
2021-12-19 15:00:18 -0700

    shooting my shot

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           THIS SITE IS UNDER CONSTRUCTION
-                      🙂
+                      🙂<br></br>
+              Hi Eve,please scroll up 
+
         </p>
  
       </header>
+     
+      <p  > 
+        H3110 Eve, 1st i apologize if i sounded rude or annoying on the phone , i was a bit frustrated at the situation
+        as it was the 2nd time of it happening and i'd rather not call about the same thing in such a short time frame .
+        Thanks again for your help and patiences with me . At this point i would like to shoot my shot and say hey , 
+        you can email me at selvon816 at gmail as i dont have any socials ... 
+         you sounded like a really cool human id like to know so i figured why not. 
+         If you came here,read THIS and made it this far , say hi.<br></br><br></br>
+
+         i know very unorthodox , but im the type to try ,it never hurts :). <br></br> 
+         ooo also everyone calls me Richie   
+      </p>
     </div>
   );
 }
